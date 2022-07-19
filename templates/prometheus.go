25bbd93fceca415e1de165bebf7416a757bc75c3
Juan Miguel Olmo Martínez <jolmomar@redhat.com>
2022-07-19 19:06:34 +0200

    RHOBS support
    JIRA - DFMS-147
    
    Implements Prometheus remote write to RHOBS
    
    Signed-off-by: Juan Miguel Olmo Martínez <jolmomar@redhat.com>

--- templates/prometheus.go
+++ templates/prometheus.go
@@ -18,12 +18,15 @@ package templates
 
 import (
 	"fmt"
+	"strings"
 
-	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	"github.com/red-hat-storage/mcg-osd-deployer/utils"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+
+	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+
+	"github.com/red-hat-storage/mcg-osd-deployer/utils"
 )
 
 var (
@@ -37,6 +40,23 @@ var resourceSelector = metav1.LabelSelector{
 	MatchLabels: map[string]string{
 		"app": "managed-mcg",
 	},
+}
+
+var metrics = []string{
+	"noobaa_num_buckets_claims",
+	"noobaa_num_unhealthy_bucket_claims",
+	"job:noobaa_total_unhealthy_namespace_buckets",
+	"job:noobaa_namespace_bucket_count",
+	"noobaa_accounts_num",
+	"noobaa_total_usage",
+	"noobaa_num_unhealthy_namespace_resources",
+	"nooBaa_num_namespace_resources",
+	"noobaa_namespace_resource_status",
+	"noobaa_namespace_bucket_status",
+	"noobaa_system_health_status",
+	"BucketPolicyErrorState",
+	"CacheBucketErrorState",
+	"DataSourceErrorState",
 }
 
 var PrometheusTemplate = promv1.Prometheus{
@@ -95,6 +115,28 @@ var PrometheusTemplate = promv1.Prometheus{
 				},
 				Resources: utils.GetResourceRequirements("kube-rbac-proxy"),
 			}},
+			RemoteWrite: []promv1.RemoteWriteSpec{
+				{
+					OAuth2: &promv1.OAuth2{
+						ClientSecret: corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{},
+						},
+						ClientID: promv1.SecretOrConfigMap{
+							Secret: &corev1.SecretKeySelector{
+								LocalObjectReference: corev1.LocalObjectReference{},
+							},
+						},
+						EndpointParams: map[string]string{},
+					},
+					WriteRelabelConfigs: []promv1.RelabelConfig{
+						{
+							SourceLabels: []promv1.LabelName{"__name__"},
+							Regex:        strings.Join(metrics, "|"),
+							Action:       "keep",
+						},
+					},
+				},
+			},
 		},
 		RuleSelector: &resourceSelector,
 		Alerting: &promv1.AlertingSpec{
